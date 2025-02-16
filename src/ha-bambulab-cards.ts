c6f51996a13ec6b8afb1a6a3492214a0f22bdacb
Greg Hesp <greg.hesp@gmail.com>
2025-02-16 11:21:30 +0000

    Update log line

--- src/ha-bambulab-cards.ts
+++ src/ha-bambulab-cards.ts
@@ -4,4 +4,5 @@ import "./cards/print-control-card/print-control-card";
 import "./cards/print-status-card/print-status-card";
 // import "./cards/example-card/example-card";
 
-console.info(`%c🐼 Bambu Lab 🐼 - ${version}`, "color: #ef5350; font-weight: 700;");
+console.info(`%c🐼 Bambu Lab 🐼 %c ${version}`, 'color: #ffffff; background:rgb(109, 109, 109); padding: 5px; font-size: 1.2em;','color:rgb(255, 255, 255); background: #22A041; font-size: 1.2em; padding: 5px')
+
